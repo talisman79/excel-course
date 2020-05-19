f6ae43f4744cffa3bf275eeffe1c43f058410659
anatoliy <ka_2005@mail.ru>
2020-05-19 23:07:05 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,38 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown']
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
+
   toHTML() {
     return createTable(20);
+  }
+
+  onClick() {
+    console.log('click');
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
+  }
+
+  onMousemove() {
+    console.log('mousemove');
+  }
+
+  onMouseup() {
+    console.log('mouseup');
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,30 +1,40 @@
-const CODE = {
+const CODES = {
   A: 65,
   Z: 90
 };
 
-function toCell() {
-  return `<div class="cell" contenteditable></div>`;
+function toCell(_, col) {
+  return `<div class="cell" contenteditable data-col="${col}"></div>`;
 }
 
-function toColumn(col) {
-  return `<div class="column">${col}</div>`;
+function toColumn(col, index) {
+  return `
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+     </div>`;
 }
 
 function createRow(index, content) {
+  const resize = index ?
+    '<div class="row-resize" data-resize="row"></div>' :
+    '';
   return `
-    <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>`;
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODE.A + index);
+  return String.fromCharCode(CODES.A + index);
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODE.Z - CODE.A + 1;
+  const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
 
   const cols = new Array(colsCount)
